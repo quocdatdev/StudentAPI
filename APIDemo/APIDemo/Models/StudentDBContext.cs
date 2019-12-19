e0f2504e5be1be400dcbd51ac7b8f767493c6c81
vinanavy <vinanavy@gmail.com>
2019-12-19 19:41:14 +0700

    Install Entity and add DBContext

---
+++ APIDemo/APIDemo/Models/StudentDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace APIDemo.Models
+{
+    public class StudentDBContext : DbContext
+    {
+        DbSet<Student> Students { get; set; }
+    }
+}
