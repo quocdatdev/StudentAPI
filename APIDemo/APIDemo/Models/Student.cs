2f3525907e60185c27452c7f9d65d57dc4ba4574
vinanavy <vinanavy@gmail.com>
2019-12-19 19:32:50 +0700

    Create student model

---
+++ APIDemo/APIDemo/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIDemo.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Mail { get; set; }
+        
+        public string Phone { get; set; }
+    }
+}
